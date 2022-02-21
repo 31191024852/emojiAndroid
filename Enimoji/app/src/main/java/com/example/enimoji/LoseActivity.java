940fb31d45585f3d82f2d7e437c5a063f896ef04
Tân Larva <tanphan.31191024852@st.ueh.edu.vn>
2022-02-22 02:16:31 +0700

    emoji
    Phan Duy Tân

---
+++ Enimoji/app/src/main/java/com/example/enimoji/LoseActivity.java
@@ -0,0 +1,25 @@
+package com.example.enimoji;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoseActivity extends AppCompatActivity {
+    Button btLose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose);
+
+        btLose = findViewById(R.id.btLose);
+        btLose.setOnClickListener(view -> {
+            Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+            startActivity(myIntent);
+        });
+    }
+
+
+}
